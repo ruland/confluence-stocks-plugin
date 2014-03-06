d6c7f14d6770f9fb1d06e8049d1b888ae995e6fd
Roeland Molenaar <rmolenaar@atlassian.com>
2014-03-06 10:57:15 +0100

    load prices on initial load, use timeout instead of interval

--- src/main/resources/js/links-stock.js
+++ src/main/resources/js/links-stock.js
@@ -1,28 +1,29 @@
 AJS.toInit(function($) {
 	AJS.$('a[href*="finance.yahoo.com"]').addClass("stocks aui-lozenge aui-lozenge-subtle");
 
-  window.setInterval(function(){
-  		/// 
-      AJS.$.each($(".stocks").get(), function(i, item) {
-        var symbol = item.href.substr(29);
-        var quote = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
-        var url = "http://query.yahooapis.com/v1/public/yql?q=" + quote + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env";
-        AJS.$.ajax({
-          type: "GET",
-          dataType: "json",
-          url: url,
-          success: function(data) {
-            stock = data.query.results.quote;
-            price = stock.LastTradePriceOnly;
-            percent = stock.Change_PercentChange;
-            item.innerHTML = symbol + " <b>" + price + "</b> [" + percent + "]";
-            if (percent.charAt(0) == "+") {
-              item.className = "stocks aui-lozenge aui-lozenge-success aui-lozenge-subtle";
-            } else {
-              item.className = "stocks aui-lozenge aui-lozenge-error aui-lozenge-subtle";
-            }
-          }  
-        });
-      });  
-	}, 60000);
+  (function updateStocks() {
+  	// Create and update price information for stocks
+    AJS.$.each($(".stocks").get(), function(i, item) {
+      var symbol = item.href.substr(29);
+      var quote = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+      var url = "http://query.yahooapis.com/v1/public/yql?q=" + quote + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env";
+      AJS.$.ajax({
+        type: "GET",
+        dataType: "json",
+        url: url,
+        success: function(data) {
+          stock = data.query.results.quote;
+          price = stock.LastTradePriceOnly;
+          percent = stock.Change_PercentChange;
+          item.innerHTML = symbol + " (" + stock.Name + ") <b>" + price + "</b> [" + percent + "]";
+          if (percent.charAt(0) == "+") {
+            item.className = "stocks aui-lozenge aui-lozenge-success aui-lozenge-subtle";
+          } else {
+            item.className = "stocks aui-lozenge aui-lozenge-error aui-lozenge-subtle";
+          }
+        }  
+      });
+    });
+    setTimeout(updateStocks, 60000);  
+	})();
 });
