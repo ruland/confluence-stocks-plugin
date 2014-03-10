7ac4ef1eec3b8148a0e578f765adc47e4b67b286
Roeland Molenaar <rmolenaar@atlassian.com>
2014-03-10 18:18:56 +0100

    fixed fault condition and added graphs

--- src/main/resources/js/links-stock.js
+++ src/main/resources/js/links-stock.js
@@ -7,6 +7,20 @@ AJS.toInit(function($) {
       var symbol = item.href.substr(29);
       var quote = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
       var url = "http://query.yahooapis.com/v1/public/yql?q=" + quote + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env";
+      var chartUrl = "http://chart.finance.yahoo.com/z?s=" + symbol + "&t=3m&q=l&l=on&z=s&p=m50"
+      AJS.InlineDialog(AJS.$( $(this) ), 
+        "Chart", 
+        function(content, trigger, showPopup) {
+          content.css({"padding":"20px"}).html('<img src="' + chartUrl + '" />');
+          showPopup();
+          return false;
+        },
+        {
+          onHover: true,
+          noBind: true,
+          width: 340,
+        }
+      );
       AJS.$.ajax({
         type: "GET",
         dataType: "json",
@@ -19,7 +33,7 @@ AJS.toInit(function($) {
             item.innerHTML = symbol + " (" + stock.Name + ") <b>" + price + "</b> [" + percent + "]";
             if (percent.charAt(0) == "+") {
               item.className = "stocks aui-lozenge aui-lozenge-success aui-lozenge-subtle";
-            } else {
+            } else if (percent.charAt(0) == "-") {
               item.className = "stocks aui-lozenge aui-lozenge-error aui-lozenge-subtle";
             }
           }
